6bb4f9a4df15986b944a4f3dcdd3128203894c17
Riley <24r.waghorn@thelenham.viat.org.uk>
2025-10-31 16:13:09 +0000

    yayayaayayaya

--- mac.sh
+++ mac.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 set -e
 
-TARGET="FNaF N.I.G.H.T.S."
+TARGET="Nights"
 
 echo "Cleaning previous build..."
 make clean
@@ -12,5 +12,19 @@ make all
 echo "Creating macOS ARM64 bundle..."
 make macos-arm
 
+echo "Renaming bundle to $TARGET-arm64.app"
+mv bundle/$TARGET.app bundle/$TARGET-arm64.app
+
 echo "Creating macOS x86_64 bundle..."
 make macos-intel
+
+echo "Renaming bundle to $TARGET-x86_64.app"
+mv bundle/$TARGET.app bundle/$TARGET-x86_64.app
+
+echo "Creating universal bundle..."
+make macos-fat
+
+echo "Renaming bundle to $TARGET-universal.app"
+mv bundle/$TARGET.app bundle/$TARGET-universal.app
+
+echo "All bundles built successfully."
